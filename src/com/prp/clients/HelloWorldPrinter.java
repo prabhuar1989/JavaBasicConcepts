83cca3384f599e26ccfe9b3f61b6a43f99117ccb
Ashwin <prabhuar1989@gmail.com>
2018-11-24 13:46:43 +0530

    First commit

---
+++ src/com/prp/clients/HelloWorldPrinter.java
@@ -0,0 +1,11 @@
+package com.prp.clients;
+
+public class HelloWorldPrinter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
